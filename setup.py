a6f621a498b73a2dc026fc529f996e1982467d09
mikekestemont <mike.kestemont@gmail.com>
2019-01-10 09:58:51 +0100

    make installable

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="stresser",
+    version="0.0.1",
+    description="Stresser",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
